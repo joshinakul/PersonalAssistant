d8344208f666bc70ae2cfdc3f987a3553e57ba85
joshinakul <nakulj3010@outlook.com>
2019-02-05 11:34:02 +0530

    extraction of faces from image

---
+++ extract_faces.py
@@ -0,0 +1,6 @@
+import cv2
+img = cv2.imread('Data/nj.jpg',0)
+cascade = cv2.CascadeClassifier('Haarcascade/myfacehaarcascade.xml')
+faces = cascade.detectMultiScale(img,1.5,4)
+for (x,y,w,h) in faces:
+    cv2.imwrite('Extracted_faces/nj.jpg',img[y:y+h,x:x+w])
