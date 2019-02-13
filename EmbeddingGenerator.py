439f5d74bf8d81eaad4c2a938d87cf03dd301d41
jatinkhemchandani <kj5699>
2019-02-13 22:58:13 +0530

    Add Model Architecture

--- EmbeddingGenerator.py
+++ EmbeddingGenerator.py
@@ -5,5 +5,16 @@ Created on Thu Feb  7 09:47:26 2019
 
 @author: jk
 """
+from keras.models import load_model
 
-b
+class embedding():
+    
+    def __init__(self):
+        # load model 
+        self.model=load_model('facenet_keras.h5')
+    
+    def embeddings(self,img):
+        # return prediction vector of 128 values
+        return self.model.predict(img)[0]
+    
+        
--- ModelArchitecture.py
+++ ModelArchitecture.py
@@ -6,3 +6,28 @@ Created on Thu Feb  7 09:47:25 2019
 @author: jk
 """
 
+from keras.layers import Dense,Activation
+from keras.layers import LeakyReLU
+from keras.models import Sequential
+
+class Model():
+    
+    def __init__(self,classes):
+        
+        self.model=Sequential()
+        self.classes=classes
+        
+    
+    def model(self):
+        
+        self.model.add(Dense(64,input_dim=128))
+        self.model.add(LeakyReLU(alpha=0.1))
+        self.model.add(Dense(32))
+        self.model.add(LeakyReLU(alpha=0.1))
+        self.model.add(Dense(16))
+        self.model.add(LeakyReLU(alpha=0.1))
+        self.model.add(Dense(self.classes))
+        self.model.add(Activation('softmax'))
+        
+        return self.model
+        
