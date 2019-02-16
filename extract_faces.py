62baef93d77b1e9dbe2a18bc16e6126ead0d61d4
joshinakul <nakulj3010@outlook.com>
2019-02-17 02:25:49 +0530

    updated data generator file

--- extract_faces.py
+++ extract_faces.py
@@ -1,6 +1,21 @@
 import cv2
-img = cv2.imread('Data/nj.jpg')
 cascade = cv2.CascadeClassifier('Haarcascade/myfacehaarcascade.xml')
-faces = cascade.detectMultiScale(img,1.5,4)
-for (x,y,w,h) in faces:
-    cv2.imwrite('Extracted_faces/nj.jpg',img[y:y+h,x:x+w])
+
+vc = cv2.VideoCapture(0)
+i = 0
+while True:
+    ret,frame = vc.read(0)
+    faces = cascade.detectMultiScale(frame,1.5,4)
+    
+    for (x,y,w,h) in faces:
+        i+=1
+        cv2.imwrite('Extracted_faces/'+str(i)+'.jpg',frame[y:y+h,x:x+w])
+        
+        cv2.waitKey(100)
+        
+    cv2.imshow('frame',frame)
+    cv2.waitKey(30)
+    if i>49:
+        break
+vc.release()
+cv2.destroyAllWindows()
